cc1df2465ee191f123ac55fe04fcb9dfc7782cc5
haidarvm <haidarvm@gmail.com>
2020-10-06 15:30:03 +0700

    repair some

---
+++ application/views/about.php
@@ -0,0 +1,46 @@
+    <div class="site-section">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-10">
+                    <div class="heading mb-4">
+                        <span class="caption">About us</span>
+                        <h2>ABOUT REPOSITORY FISIP UNLA</h2>
+                    </div>
+                    <p>Repositori Fakultas Ilmu Sosial dan Ilmu Politik (Fisip) Universitas Langlangbuana (Unla)
+                        merupakan
+                        repository institusi yang memiliki tujuan untuk melestarikan aset intelektual yang dihasilkan
+                        oleh
+                        Fisip Unla serta memastikan aset tersebut dapat diakses terus dan mudah. Keberadaan Repository
+                        Fisip
+                        Unla ini diharapkan dapat memberi kontribusi yang signifikan terhadap perkembangan ilmu
+                        pengetahuan
+                        terutama bidang pendidikan dalam lingkup nasional maupun internasional.
+                        Aset intelektual yang dikelola pada Repository Unla saat ini meliputi:</p>
+                    <ul style="list-style: none;">
+                        <li>1) tugas akhir mahasiswa berupa skripsi, tesis, dan disertasi;</li>
+                        <li>2) artikel ilmiah sivitas akademika;</li>
+                        <li>3) prosiding dari beberapa seminar dan konferensi baik penyelenggaraan dari dalam ataupun
+                            dari
+                            luar.</li>
+                        <li>4) RPS, silabus, e-book</li>
+                    </ul>
+
+                    <p>PENGAKSESAN</p>
+                    <p>Pengaksesan fulltext terhadap data yang sudah terunggah pada Repository Fisip Unla hanya dapat
+                        dilakukan di lingkungan Unla. Pengguna yang mengakses Repository Fisip Unla di luar lingkungan
+                        Unla
+                        dapat mengakses secara fulltext hanya file judul,file Bab I, III, V, dan file daftar pustaka.
+                    </p>
+
+                    <p>SOFTWARE</p>
+                    <p>Server yang digunakan dalam pengelolaan Repository Fisip Unla adalah milik Unla. </p>
+
+                    <p>KONTAK KAMI</p>
+                    Fakultas Ilmu Sosial dan Ilmu Politik Universitas Langlangbuana
+                    JL. Karapitan No. 116 Bandung 40154
+                    Telp 022-4205071
+                    Email: fisip.unla.ac.id, fisipunlabandung@gmail.com </p>
+                </div>
+            </div>
+        </div>
+    </div>
