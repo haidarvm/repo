e71ff398f6e752a6c686473529801e059f02748f
haidarvm <haidarvm@gmail.com>
2021-01-15 12:00:09 +0700

    repair pdf preview

--- application/views/view.php
+++ application/views/view.php
@@ -1,15 +1,18 @@
 <div class="site-section">
-    <div class="container ">
-        <div class="row ">
+    <div class="container">
+        <div class="row">
             <div class="col">
                 <h2 class="text-dark"><?=$repo->title;?></h2>
                 <p><?=$repo->author;?>, <?=year($repo->date);?> <em><?=$repo->title;?></em>
-            <?=ucfirst($repo->subject_name);?></p>
+                    <?=ucfirst($repo->subject_name);?></p>
             </div>
         </div>
-        <div class="col-lg-4">
-            <div class="box small h-100 mb-4">
-                <?php foreach($files as $file) { ?>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="box small h-100 mb-4">
+                    <?php foreach($files as $file) { ?>
+
                     <div class="d-flex align-items-center mb-2">
                         <div class="img"><img src="<?=base_url();?>assets/img/PDF.png" alt="doc">
                         </div>
@@ -64,55 +67,51 @@
                             </div>
                         </div>
                     </div>
-                <?php } ?>
+                    <?php } ?>
+                </div>
+                <!-- Button trigger modal -->
             </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <p>Abstract</p>
-            </div>
-        </div>
-        <div class="row">
-            <p><?=$repo->abstract;?></p>
-        </div>
-        <div class="row">
-            <div class="col-2">
-                Author:
-            </div>
-            <div class="col-6">
-                <?=$repo->author;?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-2">
-                Prodi Type:
-            </div>
-            <div class="col-6">
-                <?=$repo->prodi_name;?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-2">
-                Item Type:
-            </div>
-            <div class="col-6">
-                <?=$repo->name;?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-2">
-                Subject:
-            </div>
-            <div class="col-6">
-                <?=$repo->subject_name;?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-2">
-                Date:
-            </div>
-            <div class="col-6">
-                <?=year($repo->date);?>
+            <div class="col-lg-8">
+                <div class="row">
+                    <div class="col">
+                        <p>Abstract</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <p><?=$repo->abstract;?></p>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Author:
+                    </div>
+                    <div class="col-6">
+                        <?=$repo->author;?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Item Type:
+                    </div>
+                    <div class="col-6">
+                        <?=$repo->name;?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Subject:
+                    </div>
+                    <div class="col-6">
+                        <?=$repo->subject_name;?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Date:
+                    </div>
+                    <div class="col-6">
+                        <?=year($repo->date);?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
